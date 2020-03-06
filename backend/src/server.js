e0f457a91e40c3f70e56ec60f39b2b2615f1b724
ThiagoSVieira <thiagosilvavieira97@gmail.com>
2020-03-05 23:46:24 -0300

    Changed schema mongo for return thumbnail url.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://aircnc.mongo:27017/omnistack', {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
